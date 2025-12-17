906903fb81567429d6cc8209f2ac7a1bc92e2a66
Fabian Morón Zirfas <hash@ff6347.email>
2025-12-17 07:30:06 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,121 @@
+
+#include <Arduino.h>
+
+uint8_t readCapacitivePin(int pinToMeasure);
+
+// this is fully based on this instructables
+// http://www.instructables.com/id/Turn-a-pencil-drawing-into-a-capacitive-sensor-for/?ALLSTEPS
+// the only thing I changed is the LED pin
+//
+// Pin for the LED
+int LEDPin = 13;
+// Pin to connect to your drawing
+int capSensePin = 2;
+// This is how high the sensor needs to read in order
+//  to trigger a touch.  You'll find this number
+//  by trial and error, or you could take readings at
+//  the start of the program to dynamically calculate this.
+int touchedCutoff = 40;
+int max_val = 0;
+int min_val = 1000;
+#define CYCLES 16000
+
+void setup(){
+  Serial.begin(9600);
+  // Set up the LED
+  pinMode(LEDPin, OUTPUT);
+  digitalWrite(LEDPin, LOW);
+}
+
+void loop(){
+  // If the capacitive sensor reads above a certain threshold,
+  //  turn on the LED
+  if (readCapacitivePin(capSensePin) > touchedCutoff) {
+    digitalWrite(LEDPin, HIGH);
+  }
+  else {
+    digitalWrite(LEDPin, LOW);
+  }
+
+  // Every 500 ms, print the value of the capacitive sensor
+  if ( (millis() % 100) == 0){
+
+  int val = readCapacitivePin(capSensePin);
+  if(max_val < val){
+  max_val = val;
+  }
+  if(min_val > val){
+  min_val = val;
+  }
+    // Serial.print("Capacitive Sensor on Pin 2 reads: ");
+    Serial.print(val);
+    Serial.print(",");
+    Serial.print(min_val);
+    Serial.print(",");
+    Serial.println(max_val);
+  }
+}
+
+// readCapacitivePin
+//  Input: Arduino pin number
+//  Output: A number, from 0 to 17 expressing
+//          how much capacitance is on the pin
+//  When you touch the pin, or whatever you have
+//  attached to it, the number will get higher
+//  In order for this to work now,
+// The pin should have a 1+Megaohm resistor pulling
+//  it up to +5v.
+uint8_t readCapacitivePin(int pinToMeasure){
+  // This is how you declare a variable which
+  //  will hold the PORT, PIN, and DDR registers
+  //  on an AVR
+  volatile uint8_t* port;
+  volatile uint8_t* ddr;
+  volatile uint8_t* pin;
+  // Here we translate the input pin number from
+  //  Arduino pin number to the AVR PORT, PIN, DDR,
+  //  and which bit of those registers we care about.
+  byte bitmask;
+  if ((pinToMeasure >= 0) && (pinToMeasure <= 7)){
+    port = &PORTD;
+    ddr = &DDRD;
+    bitmask = 1 << pinToMeasure;
+    pin = &PIND;
+  }
+  if ((pinToMeasure > 7) && (pinToMeasure <= 13)){
+    port = &PORTB;
+    ddr = &DDRB;
+    bitmask = 1 << (pinToMeasure - 8);
+    pin = &PINB;
+  }
+  if ((pinToMeasure > 13) && (pinToMeasure <= 19)){
+    port = &PORTC;
+    ddr = &DDRC;
+    bitmask = 1 << (pinToMeasure - 13);
+    pin = &PINC;
+  }
+  // Discharge the pin first by setting it low and output
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+  delay(1);
+  // Make the pin an input WITHOUT the internal pull-up on
+  *ddr &= ~(bitmask);
+  // Now see how long the pin to get pulled up
+  int cycles = CYCLES;
+  for(int i = 0; i < cycles; i++){
+    if (*pin & bitmask){
+      cycles = i;
+      break;
+    }
+  }
+  // Discharge the pin again by setting it low and output
+  //  It's important to leave the pins low if you want to
+  //  be able to touch more than 1 sensor at a time - if
+  //  the sensor is left pulled high, when you touch
+  //  two sensors, your body will transfer the charge between
+  //  sensors.
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+
+  return cycles;
+}
